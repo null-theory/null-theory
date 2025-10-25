fd0206e4fa4b0b3ae0184bc9804d272fed0cefe4
patoshim3 <patimaactposhta@gmail.com>
2025-10-25 19:29:43 +0500

    Изменение README.md

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import YAML from "yamljs";
 import cors from 'cors';
 import routerUser from "./routers/routerUser.js";
 import authRoutes from "./routers/authRoutes.js";
 import dotenv from 'dotenv';
 import tourRouter from "./routers/tourRouter.js";
-import swaggerUi from "swagger-ui-express";
 dotenv.config();
 
 const app = express();
@@ -13,8 +11,6 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const swaggerDocument = YAML.load("./swagger.yaml");
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', routerUser);
 app.use('/api/auth', authRoutes);
 app.use("/api", tourRouter);
